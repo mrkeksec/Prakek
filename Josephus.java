e76a99924d468a564e865a57b85b3fe98f662491
Klim <klim.lestsenko@gmail.com>
2019-09-08 21:55:01 +0300

    Final

--- Josephus.java
+++ Josephus.java
@@ -1,15 +1,17 @@
-import java.util.List;
+import java.util.*;
 
 public class Josephus {
   public static <T> List<T> josephusPermutation(final List<T> items, final int k) {
     int cnt = k;
-    List<T> arr = items, newArr = new ArrayList<>();
+    List<T> arr = items;
+    List<T>newArr = new ArrayList<>();
     while (arr.size() > 0) {
       for (int i = 0; i < arr.size(); i++) {
         if (cnt == 0) {
           newArr.add(arr.get(i));
           arr.remove(i);
           cnt = k;
+          break;
         }
         cnt--;
       }
